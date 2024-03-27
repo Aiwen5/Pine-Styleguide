9087d95ef5ee3c7021aea05f4ca659dc4ef7f9ea
Evan Schatz <eeschatz@icloud.com>
2024-03-26 20:54:05 -0700

    initial commit

--- .history/pages/index_20240326204649.js
+++ .history/pages/index_20240326204649.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}>
+      <main className={`${styles.main} ${inter.className}`}>
         
       </main>
     </>
