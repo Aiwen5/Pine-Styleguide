1d5fa3c7a10636116471d4e44540f224e3012187
danielder10 <144720513+danielder10@users.noreply.github.com>
2024-04-02 18:13:27 -0700

    general layout and added nav

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+      <header className={styles.navbar}>
+        <nav>
+          <ul>
+            <li><a href="#p1">Custom Logo</a></li>
+            <li><a href="#p2">Colour Palette</a></li>
+            <li><a href="#p3">Typography</a></li>
+            <li><a href="#p4">References</a></li>
+          </ul>
+        </nav>
+      </header>
         <section className={styles.p1}>
           <h1>Custom Logo, Logo Variations and Description</h1>
+          <p>Our logo represent a pine tree thus the use for the word "Pine" as the name suggests the logo is more of a pun while the chef hat aims to show that this is also a food app</p>
+          <div className={styles.imageRow}>
+            <img className={styles.logoImage} src="/placeholder.jpeg" alt="Logo1" />
+            <img className={styles.logoImage} src="/placeholder.jpeg" alt="Logo2" />
+            <img className={styles.logoImage} src="/placeholder.jpeg" alt="Logo3" />
+          </div>
         </section>
         <section className={styles.p2}>
           <h1>Colour Palette, Colour Accessibility</h1>
+          <div className={styles.gridContainer}>
+          <div className={styles.imageRow2}>
+          <img className={styles.colorImage} src="/placeholder.jpeg" alt="Color1" />
+          <img className={styles.colorImage} src="/placeholder.jpeg" alt="Color2" />
+          <img className={styles.colorImage} src="/placeholder.jpeg" alt="Color3" />
+          <img className={styles.colorImage} src="/placeholder.jpeg" alt="Color4" />
+          </div>
+          <div className={styles.descriptionRow}>
+          <p>description: choosing color</p>
+          <p>description: choosing color</p>
+          <p>description: choosing color</p>
+          <p>description: choosing color</p>
+          </div>
+          </div>
         </section>
         <section className={styles.p3}>
           <h1>Typography, Imagery, Graphics, etc.</h1>
