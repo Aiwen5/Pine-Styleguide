98d387945b560c93c2ca30363ba37e0fdf26cea3
danielder10 <dder10@my.bcit.ca>
2024-04-07 01:55:42 -0700

    more bug fixing and styling changes

--- pages/ColorPalette.js
+++ pages/ColorPalette.js
@@ -43,6 +43,20 @@ export default function ColourPalette() {
           </p>
           </div>
           </div>
+          <div className={styles.Rationale}>
+          <h3>Rationale:</h3>
+          <p>Our color palette is hand-selected aiming to resonate with our intended theme and convey specific meanings. The neutral colors we used also ensured that people with color blindness will not have difficulty finding or reading our buttons & text</p>
+<p>Our primary color, #2B3F47, is an earthy tone reminiscent of a lush forest, symbolizing our logo theme. This hue serves as the foundation of our brand identity, anchoring our app's visual presence with a sense of groundedness and reliability.</p>
+
+<p>Complementing this primary shade, our secondary color, #EFFFF8, offers a clean contrast. It gives our palette a sense of clarity, cleanliness, and openness, aligning with our commitment to providing users with transparent and wholesome food experiences.</p>
+
+<p>Our thirdenary color, #C2EEE1, introduces a soft, soothing tone. This hue adds depth and dimension to our palette, this soft tone resonates with our app's goal of promoting holistic well-being and mindful consumption. </p>
+
+<p>Lastly, our button color, #46757C, is carefully chosen to command attention and prompt action. Its rich, teal-like hue encourages users to engage with our app's features and explore the culinary possibilities it offers. </p>
+
+<p>Together, our color palette embodies our brand's values and aspirations, friendly to all users who wish to expand and explore their culinary discoveries and experiences.
+</p>
+</div>
         </section>
         </main>
   );
--- pages/LogoFavicon.js
+++ pages/LogoFavicon.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from '@/styles/LogoFavicon.module.css';
 
 export default function LogoFavicon() {
@@ -6,32 +5,45 @@ export default function LogoFavicon() {
     <main className={styles.main}>
       <header className={styles.navbar}>
         <nav>
-        <ul>
-          <li><a href='/'><img className={styles.NavLogo} src='PINE logo.png'></img></a></li>
-            <li><a href="/LogoFavicon">Logo & Favicon</a></li>
-            <li><a href="/ColorPalette">Colour Palette</a></li>
-            <li><a href="/Typography">Typography</a></li>
-            <li><a href="/References">References</a></li>
+          <ul>
+            <li>
+              <a href='/'>
+                <img className={styles.NavLogo} src='PINE logo.png' alt='Logo' />
+              </a>
+            </li>
+            <li><a href='/LogoFavicon'>Logo & Favicon</a></li>
+            <li><a href='/ColorPalette'>Colour Palette</a></li>
+            <li><a href='/Typography'>Typography</a></li>
+            <li><a href='/References'>References</a></li>
           </ul>
         </nav>
       </header>
-    <section className={styles.p1} id='p1'>
-          <h1>Custom Logo, Logo Variations and Description</h1>
-          <p>Our logo represent a pine tree thus the use for the word "Pine" as the name suggests the logo is more of a pun while the chef hat aims to show that this is also a food app</p>
-          <div className={styles.imageRow}>
-            <img className={styles.logoImage} src="/PINE logo.png" alt="Logo1" />
-            <img className={styles.logoImage2} src="/BlackWhite.svg" alt="Logo2" />
-            <img className={styles.logoImage3} src="/Fancy.svg" alt="Logo3" />
+      <section className={styles.p1} id='p1'>
+        <h1>Custom Logo, Logo Variations and Description</h1>
+        <p>Our logo represents a pine tree, hence the use of the word "Pine." It's a playful pun, and the addition of the chef hat subtly hints at our culinary focus, making it clear that our app is all about food.</p>
+        <h3>Logos</h3>
+        <div className={styles.imageRow}>
+          <div>
+            <img className={styles.logoImage} src='/PINE logo.png' alt='Logo1' />
+            <p>Default</p>
           </div>
-          <div className={styles.descriptionRow}>
-          <p>Description: Choosing Logo1 hudhiahdiahidhaihdiah dihaihdiahdiahid hiahdiahidhaidhi</p>
-          <p>Description: Choosing Logo2 hudhiahdiahidhaihdiah dihaihdiahdiahid hiahdiahidhaidhi</p>
-          <p>Description: Choosing Logo3 hudhiahdiahidhaihdiah dihaihdiahdiahid hiahdiahidhaidhi</p>
+          <div>
+            <img className={styles.logoImage2} src='/PINE logo.png' alt='Logo2' />
+            <p>GreyScale</p>
           </div>
-          <h3>Favicon</h3>
-          <img className={styles.favicon} src="/favicon.ico" alt="Favicon" />
-          <p>We chose to simplify the logo by using the recognizable vector image of the pine tree and chef hat which symbolizes the app name PINE and the use by adding the chef hat</p>
-        </section>
-        </main>
+          <div>
+            <img className={styles.logoImage3} src='/PINE logo.png' alt='Logo3' />
+            <p>MonoChromatic</p>
+          </div>
+          <div>
+            <img className={styles.logoImage4} src='/PINE logo.png' alt='Logo4' />
+            <p>Reversed</p>
+          </div>
+        </div>
+        <h3>Favicon</h3>
+        <img className={styles.favicon} src='/favicon.ico' alt='Favicon' />
+        <p>We chose to simplify the logo by using the recognizable vector image of the pine tree and chef hat, symbolizing the app name PINE and the use by adding the chef hat.</p>
+      </section>
+    </main>
   );
 }
